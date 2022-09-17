00fd70d2c842dbdd46be726551ff43804a71e8b5
Johny Hoffman <johny.hoffman@gmail.com>
2022-09-17 17:12:00 -0500

    add systeminfo type

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -4,10 +4,12 @@ import (
 	"net/http"
 )
 
+type systemInfo map[string]string
+
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	env := envelope{
 		"status": "available",
-		"system_info": map[string]string{
+		"system_info": systemInfo{
 			"environment": app.config.env,
 			"version":     version,
 		},
