0310d7f41547f1bdf54f8497a59da413110dbe95
Johny Hoffman <johny.hoffman@gmail.com>
2022-10-04 08:06:01 -0500

    add activation token

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/eclecticjohny/greenlight/internal/data"
 	"github.com/eclecticjohny/greenlight/internal/validator"
@@ -53,8 +54,19 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	app.background(func() {
-		err = app.mailer.Send(user.Email, "user_welcome.gohtml", user)
+		data := map[string]any{
+			"activationToken": token.Plaintext,
+			"userID":          user.ID,
+		}
+
+		err = app.mailer.Send(user.Email, "user_welcome.gohtml", data)
 		if err != nil {
 			app.logger.Error("mailer send error", zap.Error(err))
 		}
